d7cf5d260e49f9210ecd5ce23ca6329f0719fb31
N1Anoobis <mrbond84@hotmail.com>
2021-02-16 17:37:05 +0100

    Add class to create project object

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-// Project State Management
+// Project Type
+var ProjectStatus;
+(function (ProjectStatus) {
+    ProjectStatus[ProjectStatus["Active"] = 0] = "Active";
+    ProjectStatus[ProjectStatus["Finished"] = 1] = "Finished";
+})(ProjectStatus || (ProjectStatus = {}));
+var Project = /** @class */ (function () {
+    function Project(id, title, description, people, status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.people = people;
+        this.status = status;
+    }
+    return Project;
+}());
 var ProjectState = /** @class */ (function () {
     function ProjectState() {
         this.listeners = [];
@@ -15,12 +30,7 @@ var ProjectState = /** @class */ (function () {
         this.listeners.push(listenerFn);
     };
     ProjectState.prototype.addProject = function (title, description, numOfPeople) {
-        var newProject = {
-            id: Math.random().toString(),
-            title: title,
-            description: description,
-            people: numOfPeople
-        };
+        var newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
         this.projects.push(newProject);
         for (var _i = 0, _a = this.listeners; _i < _a.length; _i++) {
             var listenerFn = _a[_i];
@@ -36,21 +46,21 @@ function validate(validatableInput) {
         isValid = isValid && validatableInput.value.toString().trim().length !== 0;
     }
     if (validatableInput.minLength != null &&
-        typeof validatableInput.value === "string") {
+        typeof validatableInput.value === 'string') {
         isValid =
             isValid && validatableInput.value.length >= validatableInput.minLength;
     }
     if (validatableInput.maxLength != null &&
-        typeof validatableInput.value === "string") {
+        typeof validatableInput.value === 'string') {
         isValid =
             isValid && validatableInput.value.length <= validatableInput.maxLength;
     }
     if (validatableInput.min != null &&
-        typeof validatableInput.value === "number") {
+        typeof validatableInput.value === 'number') {
         isValid = isValid && validatableInput.value >= validatableInput.min;
     }
     if (validatableInput.max != null &&
-        typeof validatableInput.value === "number") {
+        typeof validatableInput.value === 'number') {
         isValid = isValid && validatableInput.value <= validatableInput.max;
     }
     return isValid;
@@ -72,8 +82,8 @@ var ProjectList = /** @class */ (function () {
     function ProjectList(type) {
         var _this = this;
         this.type = type;
-        this.templateElement = document.getElementById("project-list");
-        this.hostElement = document.getElementById("app");
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
         this.assignedProjects = [];
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
@@ -89,33 +99,33 @@ var ProjectList = /** @class */ (function () {
         var listEl = document.getElementById(this.type + "-projects-list");
         for (var _i = 0, _a = this.assignedProjects; _i < _a.length; _i++) {
             var prjItem = _a[_i];
-            var listItem = document.createElement("li");
+            var listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
             listEl.appendChild(listItem);
         }
     };
     ProjectList.prototype.renderContent = function () {
         var listId = this.type + "-projects-list";
-        this.element.querySelector("ul").id = listId;
-        this.element.querySelector("h2").textContent =
-            this.type.toUpperCase() + " PROJECTS";
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + ' PROJECTS';
     };
     ProjectList.prototype.attach = function () {
-        this.hostElement.insertAdjacentElement("beforeend", this.element);
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
     };
     return ProjectList;
 }());
 // ProjectInput Class
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
-        this.templateElement = document.getElementById("project-input");
-        this.hostElement = document.getElementById("app");
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.element.id = "user-input";
-        this.titleInputElement = this.element.querySelector("#title");
-        this.descriptionInputElement = this.element.querySelector("#description");
-        this.peopleInputElement = this.element.querySelector("#people");
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
         this.configure();
         this.attach();
     }
@@ -141,7 +151,7 @@ var ProjectInput = /** @class */ (function () {
         if (!validate(titleValidatable) ||
             !validate(descriptionValidatable) ||
             !validate(peopleValidatable)) {
-            alert("Invalid input, please try again!");
+            alert('Invalid input, please try again!');
             return;
         }
         else {
@@ -149,10 +159,11 @@ var ProjectInput = /** @class */ (function () {
         }
     };
     ProjectInput.prototype.clearInputs = function () {
-        this.titleInputElement.value = "";
-        this.descriptionInputElement.value = "";
-        this.peopleInputElement.value = "";
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
     };
+    // @autobind
     ProjectInput.prototype.submitHandler = function (event) {
         event.preventDefault();
         var userInput = this.gatherUserInput();
@@ -163,13 +174,13 @@ var ProjectInput = /** @class */ (function () {
         }
     };
     ProjectInput.prototype.configure = function () {
-        this.element.addEventListener("submit", this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     };
     ProjectInput.prototype.attach = function () {
-        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
     return ProjectInput;
 }());
 var prjInput = new ProjectInput();
-var activePrjList = new ProjectList("active");
-var finishedPrjList = new ProjectList("finished");
+var activePrjList = new ProjectList('active');
+var finishedPrjList = new ProjectList('finished');
